937ff3c38d728a18f997d15abe718c908fb0b477
mattuzi17 <quilaym@gmail.com>
2020-10-17 14:49:36 -0700

    update

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// function to build table display
+function buildTable(ufoData) {
+    var tbody = d3.select("tbody");
+    ufoData.forEach((ufoRecord) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoRecord).forEach((val) => {
+            var cell = row.append("td");
+            cell.text(val);    
+        });
+    });
+};
