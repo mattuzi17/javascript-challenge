915577f7a41ef560584fb32ac18fd984a00008db
mattuzi17 <quilaym@gmail.com>
2020-10-17 17:02:51 -0700

    update

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,14 +1,34 @@
 // from data.js
 var tableData = data;
 
+//get reference to the table body
+var tbody = d3.select("tbody");
+
 // function to build table display
-function buildTable(ufoData) {
-    var tbody = d3.select("tbody");
+function displayData(ufoData) {
+    tbody.html("");
     ufoData.forEach((ufoRecord) => {
         var row = tbody.append("tr");
-        Object.entries(ufoRecord).forEach((val) => {
+        Object.values(ufoRecord).forEach((val) => {
             var cell = row.append("td");
             cell.text(val);    
         });
     });
 };
+
+//initial table display
+displayData(tableData);
+
+// select filter button 
+//var button = d3.select("#filter-btn");
+//button click
+//button.on("click", function() {
+    //d3.event.preventDefault();
+    //tbody.selectAll('*').remove();
+    //var inputDate = d3.select("#datetime");
+    //var inputValue = inputDate.property("value");
+    //console.log(inputValue);
+    //var filteredData = tableData.filter(x => x.datetime === inputValue);
+    //console.log(filteredData);
+
+//}
